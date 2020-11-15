e0544574fa29b0eaf5daec317952bbfa5bd477ee
lacostea <57213564+lacostea@users.noreply.github.com>
2020-11-15 17:32:45 +0100

    Create header.php

---
+++ calendar/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<HTML>
+<head>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="/public/css/calendar.css">
+<title><?= isset($title) ? h($title) : 'Mon calendrier'; ?></title>
+</head>
+<body>
+<link rel="stylesheet" href="calendar.css">
+<nav class="navbar navbar-dark bg-primary mb-3">
+    <a href="/index.php" class="navbar-brand"> mon calandrier</a>
+</nav>
